deedd680f196e11477dde9b3663f7d3586a66f5a
Cesar Domingos <cesar.domingos@fs.com.br>
2020-07-11 22:57:47 -0300

    Teste swagger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
@@ -8,6 +9,19 @@ app = Flask(__name__)
 def hello(name=None):
     return 'Hello {}! - Version {}'.format(name if name else 'World!', os.environ.get('BUILDNO', None))
 
+
+@app.route('/docs/<path:path>')
+def doc(path):
+    return send_from_directory('static/docs', path)
+
+SWAGGER_URL = '/swagger'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    '/docs/swagger.json',
+    config={ 'app_name': 'NeoTalk Web' }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
